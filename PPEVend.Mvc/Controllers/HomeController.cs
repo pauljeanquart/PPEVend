04147e6fbb1f63a01666438842148431f2533715
Paul K. Jeanquart <pkjeanq1@HobbyLobby.corp>
2026-03-17 02:17:42 -0500

    initial code for vending poc

---
+++ PPEVend.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PPEVend.Mvc.Models;
+
+namespace PPEVend.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.Remove("ShoppingCart");
+
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            List<string> model = HttpContext.Session.GetObject<List<string>>("ShoppingCart")??(new List<string>());
+            return View(model);
+        }
+
+        public IActionResult Cart(string id)
+        {
+            List<string> shoppingCart = new List<string>();
+            List<string> sessionCart = HttpContext.Session.GetObject<List<string>>("ShoppingCart");
+            if (sessionCart != null)
+            {
+                shoppingCart.AddRange(sessionCart);
+            }
+            
+            shoppingCart.Add(id);
+            HttpContext.Session.SetObject("ShoppingCart", shoppingCart);
+
+
+            return View(shoppingCart);
+        }
+
+        public IActionResult Scan()
+        {
+            List<string> model = HttpContext.Session.GetObject<List<string>>("ShoppingCart") ?? (new List<string>());
+            return View(model);
+        }
+
+        public IActionResult Dispense()
+        {
+            List<string> model = HttpContext.Session.GetObject<List<string>>("ShoppingCart") ?? (new List<string>());
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
